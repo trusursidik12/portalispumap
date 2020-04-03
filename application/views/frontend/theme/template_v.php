e9122f426b92009981ac2acf5d0c745aa1ce2607
rizaldi <rizaldi354313@gmail.com>
2020-04-03 10:10:53 +0700

    iconbar

--- application/views/frontend/theme/template_v.php
+++ application/views/frontend/theme/template_v.php
@@ -10,7 +10,7 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="<?= base_url() ?>assets/frontend/assets/img/favicon.png" rel="icon">
+  <link href="<?= base_url() ?>assets/frontend/assets/img/ispumap.png" rel="icon">
   <link href="<?= base_url() ?>assets/frontend/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
